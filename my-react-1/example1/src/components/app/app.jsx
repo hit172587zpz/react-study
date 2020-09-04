35803536bf55c2ee0b8fa2e987a13c3f2ddd61f0
zhoupzh <zhoupzh@pcl.ac.cn>
2020-09-04 15:33:13 +0800

    the first practice

---
+++ my-react-1/example1/src/components/app/app.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import CommentAdd from '../comment-add/comment-add';
+import CommentList from '../comment-list/comment-list';
+
+export default class App extends Component {
+    // constructor(props){
+    //     super(props)
+    //     this.state={
+    //         comment: [
+    //             {username:'Tom',content:'React is very good'},
+
+    //             {username:'jack', content:"react is very bad"},
+    //         ]
+    //     }
+        
+    // }
+    state={
+        comments: [
+            {username:'Tom',content:'React is very good'},
+
+            {username:'jack', content:"react is very bad"},
+        ]
+    }
+
+
+    addComment=(comment)=>{
+        const {comments} = this.state
+        comments.unshift(comment)
+
+        this.setState({comment})
+
+    }
+
+    deleteComment=(index)=>{
+        const {comments} = this.state
+        comments.splice(index,1)
+
+        this.setState({comments})
+
+
+    }
+
+    render() {
+        const {comments}=this.state
+        
+        return (
+            <div>
+                <header className="site-header jumbotron">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-xs-12">
+                                <h1>请发表对React的评论</h1>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+
+                <div className="container">
+                    <div className="row">
+                        <CommentAdd addComment={this.addComment}/>
+                        <CommentList comments={comments} deleteComment={this.deleteComment}/>
+                    </div>            
+                </div>
+            </div>
+        )
+    }
+}
+
+
